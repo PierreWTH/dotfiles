2a50447df6666fc3596a3eb80ae7a34fd714c753
PierreWTH <wietrichpierre@gmail.com>
2025-10-28 21:14:03 +0100

    add style

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -2,9 +2,18 @@
 
 sketchybar --add item front_app left \
   --set front_app background.color="0x00000000" \
-  icon.drawing=on \
+  background.border_width=1 \
+  background.border_color="0xffffffff" \
+  background.corner_radius=5 \
+  background.height=24 \
+  background.padding_left=10 \
+  background.padding_right=10 \
   icon.color="0xffffffff" \
   icon.font="sketchybar-app-font:Regular:16.0" \
+  icon.padding_left=8 \
+  icon.padding_right=8 \
   label.color="0xffffffff" \
+  label.padding_left=8 \
+  label.padding_right=8 \
   script="$PLUGIN_DIR/front_app.sh" \
   --subscribe front_app front_app_switched
