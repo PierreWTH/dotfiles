a5fadf243e274b9205395695abf266e4cc76b2d0
PierreWTH <wietrichpierre@gmail.com>
2025-10-28 21:13:44 +0100

    replace icons

--- .config/sketchybar/icons.sh
+++ .config/sketchybar/icons.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export ICON_WIFI_CONNECTED=󰖩
+export ICON_WIFI_CONNECTED=􀙇
 
-export ICON_WIFI_OFF=󰖪
+export ICON_WIFI_OFF=􀙈
 
-export ICON_WIFI_DISCONNECTED=󱛅
+export ICON_WIFI_DISCONNECTED=􀙥
 
 export ICON_APPLE=
--- .config/sketchybar/items/spaces.sh
+++ .config/sketchybar/items/spaces.sh
@@ -19,9 +19,9 @@ for sid in $(aerospace list-workspaces --all); do
 done
 
 sketchybar --add item chevron left \
-  --set chevron icon= label.drawing=off \
+  --set chevron icon="|" label.drawing=off \
   --add item front_app left \
-  --set front_app icon.drawing=off script="$PLUGIN_DIR/front_app.sh" \
+  --set front_app script="$PLUGIN_DIR/front_app.sh" \
   --subscribe front_app front_app_switched
 
 ##### Right items #####
