5a9c18362fc5e25baf84f4cb1f4edb6d938f3250
PierreWTH <wietrichpierre@gmail.com>
2025-10-28 20:24:46 +0100

    remove debug

--- .config/sketchybar/plugins/front_app.sh
+++ .config/sketchybar/plugins/front_app.sh
@@ -2,6 +2,5 @@
 
 if [ "$SENDER" = "front_app_switched" ]; then
   ICON=$("$CONFIG_DIR/plugins/icon_map_fn.sh" "$INFO")
-  echo "$INFO $ICON $NAME" >> /tmp/sketchybar_front_app.log 
   sketchybar --set "$NAME" label="$INFO" icon="$ICON"
 fi
